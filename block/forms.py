da1c387c2706dd3cd7cbe6b9fad386471fc5614f
josephmuema <jmuema95@gmail.com>
2019-07-03 20:09:08 +0300

    modified the forms

--- block/forms.py
+++ block/forms.py
@@ -3,19 +3,32 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import *
 
-class UserRegistrationFOrm(UserCreationForm):
-  email = forms.EmailField()
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
-  class Meta:
-    models = User
-    fields = ['username','email','password1','password2']
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio','profile_pic']
 
-class NeighbourhoodForm(forms.ModelForm):
-  class Meta:
-    models = Neighbourhood
-    exclude = ['admin','occupants']
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        exclude = ['admin']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user', 'neighbourhood']
 
 class BusinessForm(forms.ModelForm):
+    class Meta:
+            model = Business
+            exclude = ['neighbourhood', 'profile']
+class NewCommentForm(forms.ModelForm):
   class Meta:
-    model = Business
-    exclude = ['person','bizhood','email']
+    model = Comment
+    exclude = ['post','postername','pub_date']
