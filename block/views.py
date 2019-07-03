be617967f50801aafd789c173d611d5213933bd6
josephmuema <jmuema95@gmail.com>
2019-07-03 16:22:13 +0300

    imported forms and models to my views

--- block/views.py
+++ block/views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
-from 
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from. models import * 
 # Create your views here.
