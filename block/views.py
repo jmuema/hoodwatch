f31445f34a7a5fa83624780ca304788a4c6763dc
josephmuema <jmuema95@gmail.com>
2019-07-03 16:28:46 +0300

    defined a registration function

--- block/views.py
+++ block/views.py
@@ -4,4 +4,18 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import *
 from. models import * 
+
 # Create your views here.
+def register(request):
+  if request.method == 'POST':
+    form = UserRegistrationFOrm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      return redirect('login')
+
+  else:
+    form = UserRegistrationFOrm()
+    return render(request, 'users/register.html', {'form' : form})
+    
+    
