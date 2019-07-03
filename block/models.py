51108b80f9f6aaddfc47bd7bc103eb554c039beb
josephmuema <jmuema95@gmail.com>
2019-07-03 16:01:10 +0300

    created a class for the neighbourhood models

--- block/models.py
+++ block/models.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+class Neighbourhood(models.Model):
+  hood_name = models.CharField(max_length =50)
+  hood_location = models.CharField(max_length =50)
+  occupants = models.IntegerField(blank=True, null=True)
+  admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__ (self):
+    return self.hood_name
+
+  def save_neighbourhood(self):
+    self.save()
+
+
