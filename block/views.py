bba3895b6b93f60685265a67f7eb2d8b401c2342
josephmuema <jmuema95@gmail.com>
2019-07-03 20:03:03 +0300

    modified the requirements

--- block/views.py
+++ block/views.py
@@ -10,7 +10,7 @@ def register(request):
   if request.method == 'POST':
     form = UserRegistrationFOrm(request.POST)
     if form.is_valid():
-      form.save()
+      form.save()nothing to commit, working tree clean
       username = form.cleaned_data.get('username')
       return redirect('login')
 
@@ -44,4 +44,20 @@ def home(request,id):
   biz = Business.objects.filter(bizhood=hood.id)
   return render(request,'home.html',{'hood':hood,'biz':biz,'user':user})
 
-    
+def biz_details(request):
+  user = request.user
+  hood = Neighbourhood.objects.get(admin=user)
+  print(hood)
+
+  if request.method == 'POST':
+    bizform= BusinessForm(request.POST)
+    if bizform.is_valid():
+      bform = bizform.save(commit=False)
+      bform.person= user
+      bform.bizhood= hood
+      bform.save()
+    return redirect('home',hood.id)
+  else:
+    bizform = BusinessForm()
+  return render(request, 'enter_biz.html',{'bizform':bizform}) 
+
