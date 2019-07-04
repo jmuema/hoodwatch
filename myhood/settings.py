7e132ae38af5184a447d45c5fadd92e674c00ddf
josephmuema <jmuema95@gmail.com>
2019-07-04 08:26:25 +0300

    fixing migration errors

--- myhood/settings.py
+++ myhood/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+from decouple import config,Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,8 +77,13 @@ WSGI_APPLICATION = 'myhood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hood',
+        'USER': 'joseph',
+        'PASSWORD': '25MuemA25',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+
     }
 }
 
