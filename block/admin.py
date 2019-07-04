2584ddf3305665c2297552b99b0538f060603732
josephmuema <jmuema95@gmail.com>
2019-07-04 07:58:14 +0300

    registered the models I created

--- block/admin.py
+++ block/admin.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Comment)
